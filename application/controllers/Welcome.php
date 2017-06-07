5840aa26e5c3b3d219677474056ed0d3410e44d1
Monique Dingding <monique.dingding@gmail.com>
2017-06-08 01:11:54 +0800

    Add files via upload
    Added new method in Welcome controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,4 +22,12 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+
+	public function testWelcome() {
+
+		echo "<pre>";
+		echo "it's my life<br>whatever i wanna do";
+		echo "</pre>";
+
+	}
 }
